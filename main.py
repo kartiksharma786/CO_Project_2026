a331a64f6ee2e1a7dbe85b88603099f9e4cb4d81
khushansingh <khushan25263@iiitd.ac.in>
2026-03-07 17:53:48 +0530

    added B type Instruction, input output file using arguments and import sys, fixed issues including labels ,added the virtual halt, added the range for i type instruction that is -2048 to 2047

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import sys
+
+input_file = sys.argv[1]
+output_file = sys.argv[2]
+
+f = open(input_file, "r")
+lines = f.readlines()
+f.close()
+
+error_found = False
+
 instr_type = {
     "R": ["add", "sub", "slt", "sltu", "xor", "sll", "srl", "or", "and"],
     "I": ["lw", "addi", "sltiu", "jalr"],
@@ -7,30 +18,16 @@ instr_type = {
     "J": ["jal"]
 }
 
-instruction_types_list = list(instr_type.keys())
-
 all_instructions = [
-    "add", "addi", "and", "auipc", "beq", "bge", "bgeu",
-    "blt", "bltu", "bne", "jal", "jalr", "lui", "lw",
-    "or", "sll", "slt", "sltiu", "sltu",
-    "srl", "sub", "sw", "xor"
+    "add","addi","and","auipc","beq","bge","bgeu",
+    "blt","bltu","bne","jal","jalr","lui","lw",
+    "or","sll","slt","sltiu","sltu","srl","sub","sw","xor"
 ]
 
-
-
-instr_to_type = {}
-for t, insts in instr_type.items():
-    for inst in insts:
+instr_to_type = {}                                     #mapping kr rha h instruction ko uske type se 
+for t in instr_type:
+    for inst in instr_type[t]:
         instr_to_type[inst] = t
-
-
-
-
-with open("instruction.asm", "r") as f:
-    lines = f.readlines()
-
-
-
 
 registers = {
     "zero": "00000","ra":"00001","sp":"00010","gp":"00011","tp":"00100",
@@ -42,25 +39,22 @@ registers = {
     "t4":"11101","t5":"11110","t6":"11111",
 }
 
-
-
-
 opcodes={
     "add":"0110011","sub":"0110011","slt":"0110011","sltu":"0110011",
     "xor":"0110011","sll":"0110011","srl":"0110011","or":"0110011","and":"0110011",
-
-    
     "sw":"0100011",
     "lui":"0110111",
-    "auipc":"0010111"
+    "auipc":"0010111",
+    "beq":"1100011","bne":"1100011","blt":"1100011",
+    "bge":"1100011","bltu":"1100011","bgeu":"1100011"
 }
 
 funct3={
     "add":"000","sub":"000","sll":"001","slt":"010","sltu":"011",
     "xor":"100","srl":"101","or":"110","and":"111",
-
-    
-    "sw":"010"
+    "sw":"010",
+    "beq":"000","bne":"001","blt":"100",
+    "bge":"101","bltu":"110","bgeu":"111"
 }
 
 funct7={
@@ -68,211 +62,209 @@ funct7={
     "xor":"0000000","srl":"0000000","or":"0000000","and":"0000000",
 }
 
+last_instruction = ""                                  
 
-
-
-output = []
-
-for line_num, line in enumerate(lines, 1):
-
+for line in reversed(lines):            #reverse order me ja rha h lines ko read krne ke liye taki last instruction mil jaye
     line = line.strip()
+
     if line == "":
         continue
+
+    if ":" in line:
+        parts = line.split(":")
+        if parts[1].strip() != "":
+            last_instruction = parts[1].strip()
+            break
+    else:
+        last_instruction = line
+        break
+
+check_line = last_instruction.replace(",", " ")
+check_parts = check_line.split()
+
+if len(check_parts) != 4 or check_parts[0] != "beq" or check_parts[1] != "zero" or check_parts[2] != "zero":        #ye check kr rha h ki last instruction virtual halt instruction hai ya nahi
+    print("ERROR: Missing virtual halt instruction")
+    sys.exit()
+
+labels = {}
+pc = 0
+
+for line_num, line in enumerate(lines,1):                        #line_num 1 se start hoga taki error messages me line number sahi aaye
+    line = line.strip()   
+
+    if line == "":
+        continue
+
+    if ":" in line:
+        label = line.split(":")[0]             #ye label ko find kr rha h line se
+
+        if label in labels:
+            print(f"ERROR line {line_num}: Duplicate label '{label}'")
+            error_found = True
+        else:
+            labels[label] = pc                #ye label ke address ko store kr rha h labels dictionary me
+
+        if line.split(":")[1].strip() != "":
+            pc += 4
+
+    else:
+        pc += 4
+
+output = []
+pc = 0
+
+for line_num, line in enumerate(lines,1):
+
+    line = line.strip()
+
+    if line == "":
+        continue
+
+    if line.endswith(":"):
+        continue
+
+    if ":" in line:
+        line = line.split(":")[1].strip()
 
     line = line.replace(",", " ")
     parts = line.split()
 
-    instr = parts[0]
-
-    
-    if instr not in all_instructions:
-        print(f"ERROR line {line_num}: Unknown instruction '{instr}'")
+    if len(parts) == 0:
         continue
 
-    
+    instr = parts[0]
+
+    if instr not in all_instructions:
+        print(f"ERROR line {line_num}: Unknown instruction '{instr}'")
+        error_found = True
+        continue
+
     t = instr_to_type[instr]
 
-    if t=="U":
-
-        if len(parts) != 3:
-            print(f"ERROR line {line_num}: U-type requires rd imm12")
+    if t == "U":
+        if len(parts) < 3:
+            print(f"ERROR line {line_num}: Missing operands")
+            error_found = True
             continue
 
-        rd=parts[1]
+        rd = parts[1]
 
         if rd not in registers:
-            print(f"ERROR line {line_num}: Invalid register in '{line}'")
+            print(f"ERROR line {line_num}: Invalid register '{rd}'")
+            error_found = True
             continue
 
-        rd_bin=registers[rd]
+        imm = int(parts[2],16)
 
-        st=""
-        for i in parts[2]:
-            st+= i
+        rd_bin = registers[rd]
+        imm_bin = bin(imm)[2:].zfill(20)
 
-        for i in st[2:len(st)]:
-            if i in "0123456789":
-                continue
-            elif i in "abcdef".upper():
-                continue
-            else:
-                print(f"ERROR line {line_num}: Invalid immediate value in '{line}'")
-                continue
+        binary = imm_bin + rd_bin + opcodes[instr]
 
-        imm=int(st[2:len(st)],16)
-
-        bin=""
-        while imm>0:
-            bin=str(imm%2)+bin
-            imm//=2
-
-        
-
-        rbin=bin
-
-        while len(rbin)<20:
-            rbin="0"+rbin
-
-        op=opcodes[instr]
-
-        binary=rbin+rd_bin+op
         output.append(binary)
 
-        print(f"Line {line_num}: {binary}")
+    elif t == "S":
 
-
-    
-    
-    
-    elif t=="S":
-
-        if len(parts) != 3:
-            print(f"ERROR line {line_num}: S-type requires rs2 imm(rs1)")
+        if len(parts) < 3:
+            print(f"ERROR line {line_num}: Missing operands")
+            error_found = True
             continue
 
-        rs2=parts[1]
+        rs2 = parts[1]
+        st = parts[2]
 
-        if rs2 not in registers:
-            print(f"ERROR line {line_num}: Invalid register in '{line}'")
-            continue    
+        imm = int(st.split("(")[0])
+        rs1 = st.split("(")[1][:-1]
 
-        rs2_bin=registers[rs2]
+        if rs1 not in registers or rs2 not in registers:
+            print(f"ERROR line {line_num}: Invalid register")
+            error_found = True
+            continue
 
-        st=""
-        for i in parts[2]:
-            st+= i
+        if imm < -2048 or imm > 2047:
+            print(f"ERROR line {line_num}: Immediate out of range")
+            error_found = True
+            continue
 
-        imm=st.split("(")[0]
-        imm=int(imm)
+        rs1_bin = registers[rs1]
+        rs2_bin = registers[rs2]
 
-        bin=""
+        imm_bin = bin((1<<12)+imm if imm<0 else imm)[2:].zfill(12)
 
-        # FIX 1 → imm = 0 case handle
+        imm1 = imm_bin[0:7]
+        imm2 = imm_bin[7:12]
+
+        binary = imm1 + rs2_bin + rs1_bin + funct3[instr] + imm2 + opcodes[instr]
+
+        output.append(binary)
+
+    elif t == "B":
+
+        if len(parts) < 4:
+            print(f"ERROR line {line_num}: Missing operands")
+            error_found = True
+            continue
+
+        rs1 = parts[1]
+        rs2 = parts[2]
+        label = parts[3]
+
+        if label.lstrip("-").isdigit():
+            offset = int(label)
+
+        else:
+            if label not in labels:
+                print(f"ERROR line {line_num}: Undefined label '{label}'")
+                error_found = True
+                continue
+
+            label_address = labels[label]
+            offset = label_address - pc
+
+        if offset < -4096 or offset > 4094:
+            print(f"ERROR line {line_num}: Branch offset out of range")
+            error_found = True
+            continue
+
+        imm = offset // 2
+
+        rs1_bin = registers[rs1]
+        rs2_bin = registers[rs2]
+
         if imm >= 0:
+            imm_bin = bin(imm)[2:].zfill(13)
+        else:
+            imm = (1<<13) + imm
+            imm_bin = bin(imm)[2:]
 
-            while imm>0:
-                bin=str(imm%2)+bin
-                imm//=2
+        imm_12 = imm_bin[0]
+        imm_10_5 = imm_bin[2:8]
+        imm_4_1 = imm_bin[8:12]
+        imm_11 = imm_bin[1]
 
-            rbin=bin
+        imm_31_25 = imm_12 + imm_10_5
+        imm_11_7 = imm_4_1 + imm_11
 
-            while len(rbin)<12:
-                rbin="0"+rbin
-        
-            imm1=rbin[0:7]
-            imm2=rbin[7:12]
+        binary = (
+            imm_31_25 +
+            rs2_bin +
+            rs1_bin +
+            funct3[instr] +
+            imm_11_7 +
+            opcodes[instr]
+        )
 
-            rs1=""
-            for i in st.split("(")[1]:
-                rs1+=i
+        output.append(binary)
 
-            rs1=rs1[:-1]
+    pc += 4
 
-            if rs1 not in registers:
-                print(f"ERROR line {line_num}: Invalid register in '{line}'")
-                continue
+if error_found == False:
 
-            rs1_bin=registers[rs1]
+    file = open(output_file, "w")
 
-            op=opcodes[instr]
-            f3=funct3[instr]
+    for line in output:
+        file.write(line + "\n")
 
-            binary=imm1+rs2_bin+rs1_bin+f3+imm2+op
+    file.close()
 
-            output.append(binary)
-
-            print(f"Line {line_num}: {binary}")
-
-        elif imm < 0:
-
-            x = -imm
-
-            while x > 0:
-                bin = str(x % 2) + bin
-                x //= 2
-
-            rbin = bin
-
-            while len(rbin) < 12:
-                rbin = "0" + rbin
-
-            
-            inv = ""
-            for i in rbin:
-                if i == "0":
-                    inv += "1"
-                else:
-                    inv += "0"
-
-            rbin = inv
-
-            
-            carry = 1
-            temp = ""
-
-            for i in rbin[::-1]:
-                if i == "1" and carry == 1:
-                    temp = "0" + temp
-                elif i == "0" and carry == 1:
-                    temp = "1" + temp
-                    carry = 0
-                else:
-                    temp = i + temp
-
-            rbin = temp
-
-            while len(rbin) < 12:
-                rbin = "0" + rbin
-
-
-            imm1 = rbin[0:7]
-            imm2 = rbin[7:12]
-
-            rs1 = ""
-            for i in st.split("(")[1]:
-                rs1 += i
-
-            rs1 = rs1[:-1]
-
-            if rs1 not in registers:
-                print(f"ERROR line {line_num}: Invalid register in '{line}'")
-                continue
-
-            rs1_bin = registers[rs1]
-
-            op = opcodes[instr]
-            f3 = funct3[instr]
-
-            binary = imm1 + rs2_bin + rs1_bin + f3 + imm2 + op
-
-            output.append(binary)
-
-            print(f"Line {line_num}: {binary}")
-
-
-
-with open("output.bin", "w") as out:
-    for b in output:
-        out.write(b + "\n")
-
-print("\nDone! Output saved to output.bin")
+    print("\nDone! Output saved to output.bin")
