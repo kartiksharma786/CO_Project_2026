fd3157a804679b7933b4a3be505def0beed86f50
kaushal25257 <kaushal25257@iiitd.ac.in>
2026-04-10 22:42:21 +0530

    Updated the logic for simulator

--- Simulator.py
+++ Simulator.py
@@ -182,3 +182,160 @@ while True:
             regs[rd] = result
 
         PC = PC + 4
+    elif opcode == "0000011":
+
+        imm_bits = instr[0:12]
+        imm_number = int(imm_bits, 2)
+        imm = sign_ext(imm_number, 12)
+
+        rs1 = int(instr[12:17], 2)
+        rd = int(instr[20:25], 2)
+
+        base = regs[rs1]
+        offset = imm
+        addr = mask32(base + offset)
+
+        if not check_memory_access(addr, rs1, PC):
+            error_found = True
+            break
+
+        val = memory.get(addr, 0)
+
+        if rd != 0:
+            regs[rd] = val
+
+        PC = PC + 4
+
+    elif opcode == "1100111":
+
+        imm_bits = instr[0:12]
+        imm_number = int(imm_bits, 2)
+        imm = sign_ext(imm_number, 12)
+
+        rs1 = int(instr[12:17], 2)
+        rd = int(instr[20:25], 2)
+
+        base = regs[rs1]
+        offset = imm
+        target = mask32(base + offset)
+
+        target = target & 0xFFFFFFFE
+
+        if rd != 0:
+            return_addr = mask32(PC + 4)
+            regs[rd] = return_addr
+
+        PC = target
+
+    elif opcode == "0100011":
+
+        upper_bits = instr[0:7]
+        lower_bits = instr[20:25]
+        imm_bits = upper_bits + lower_bits
+        imm_number = int(imm_bits, 2)
+        imm = sign_ext(imm_number, 12)
+
+        rs1 = int(instr[12:17], 2)
+        rs2 = int(instr[7:12], 2)
+
+        base = regs[rs1]
+        offset = imm
+        addr = mask32(base + offset)
+
+        if not check_memory_access(addr, rs1, PC):
+            error_found = True
+            break
+
+        value = regs[rs2] & 0xFFFFFFFF
+        memory[addr] = value
+
+        PC = PC + 4
+
+    elif opcode == "1100011":
+
+        bit_12 = instr[0]
+        bit_11 = instr[24]
+        bits_10_to_5 = instr[1:7]
+        bits_4_to_1 = instr[20:24]
+        bit_0 = '0'
+
+        imm_bits = bit_12 + bit_11 + bits_10_to_5 + bits_4_to_1 + bit_0
+        imm_number = int(imm_bits, 2)
+        imm = sign_ext(imm_number, 13)
+
+        rs1 = int(instr[12:17], 2)
+        rs2 = int(instr[7:12], 2)
+        funct3 = instr[17:20]
+
+        v1 = regs[rs1]
+        v2 = regs[rs2]
+
+        s1 = to_signed(v1)
+        s2 = to_signed(v2)
+
+        take = False
+
+        if funct3 == "000":
+            take = (v1 == v2)
+        elif funct3 == "001":
+            take = (v1 != v2)
+        elif funct3 == "100":
+            take = (s1 < s2)
+        elif funct3 == "101":
+            take = (s1 >= s2)
+        elif funct3 == "110":
+            take = ((v1 & 0xFFFFFFFF) < (v2 & 0xFFFFFFFF))
+        elif funct3 == "111":
+            take = ((v1 & 0xFFFFFFFF) >= (v2 & 0xFFFFFFFF))
+
+        if funct3 == "000" and rs1 == 0 and rs2 == 0 and imm == 0:
+            halt = True
+
+        if take:
+            PC = mask32(PC + imm)
+        else:
+            PC = PC + 4
+
+    elif opcode == "0110111":
+
+        imm = int(instr[0:20], 2)
+        rd = int(instr[20:25], 2)
+
+        if rd != 0:
+            regs[rd] = mask32(imm << 12)
+
+        PC = PC + 4
+
+    elif opcode == "0010111":
+
+        imm = int(instr[0:20], 2)
+        rd = int(instr[20:25], 2)
+
+        if rd != 0:
+            regs[rd] = mask32(PC + (imm << 12))
+
+        PC = PC + 4
+
+    elif opcode == "1101111":
+
+        bit_20 = instr[0]
+        bits_19_to_12 = instr[12:20]
+        bit_11 = instr[11]
+        bits_10_to_1 = instr[1:11]
+        bit_0 = '0'
+
+        imm_bits = bit_20 + bits_19_to_12 + bit_11 + bits_10_to_1 + bit_0
+        imm_number = int(imm_bits, 2)
+        imm = sign_ext(imm_number, 21)
+
+        rd = int(instr[20:25], 2)
+
+        if rd != 0:
+            return_addr = mask32(PC + 4)
+            regs[rd] = return_addr
+
+        PC = mask32(PC + imm)
+
+    else:
+        print("Unknown opcode:", opcode)
+        break
